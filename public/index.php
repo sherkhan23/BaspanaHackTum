f658059717f8b51ca8a089cc8513a04996804142
Sherkhan Khanseiit <57889104+sherkhan23@users.noreply.github.com>
2022-11-06 17:56:50 +0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
+
 define('LARAVEL_START', microtime(true));
 
 /*
@@ -51,5 +52,6 @@ $kernel = $app->make(Kernel::class);
 $response = $kernel->handle(
     $request = Request::capture()
 )->send();
+
 
 $kernel->terminate($request, $response);
